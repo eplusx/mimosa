e3d2a8852913a506a76b72d341eeda7b66972d37
Hiroaki Hasegawa <eplusx@gmail.com>
2023-12-29 23:08:39 +0900

    Accept command line arguments to specify the command.
    Command line arguments can be passed like:
    ```
    # On Linux
    ./gradle :app:run --args="get-devices"
    # On Windows
    .\gradlew.bat :app:run --args="get-devices"
    ```

--- app/src/main/kotlin/net/eplusx/logger/App.kt
+++ app/src/main/kotlin/net/eplusx/logger/App.kt
@@ -2,6 +2,13 @@ package net.eplusx.logger
 
 import net.eplusx.logger.switchbot.SwitchBotClient
 
-fun main() {
-    println(SwitchBotClient().getDevices().toJson())
+fun main(args: Array<String>) {
+    require(args.isNotEmpty()) { "Usage: App <command> [args...]" }
+    when (args[0]) {
+        "get-devices" -> {
+            require(args.size == 1) { "Usage: App get-devices" }
+            println(SwitchBotClient().getDevices().toJson())
+        }
+        else -> throw IllegalArgumentException("Unknown command: ${args[0]}")
+    }
 }
