d1f2eaee45545475dcd0ad5f048327223ebe15f4
Hiroaki Hasegawa <eplusx@gmail.com>
2024-01-23 21:56:26 +0900

    Adopt Ktor for accepting SwitchBot webhooks and metric updates from Rhaspberry Pi Pico in Mimosa daemon.

--- buildSrc/src/main/kotlin/mimosa.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/mimosa.kotlin-common-conventions.gradle.kts
@@ -7,6 +7,8 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 }
--- daemon/build.gradle.kts
+++ daemon/build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
-    id("mimosa.kotlin-application-conventions")
+    id("io.ktor.plugin") version "2.3.7"
     id("com.github.johnrengelman.shadow")
+
+    id("mimosa.kotlin-application-conventions")
 }
 
 group = "net.eplusx.mimosa"
@@ -18,10 +21,17 @@ dependencies {
     implementation("io.opentelemetry:opentelemetry-semconv:1.30.1-alpha")
     implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
     implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
 }
 
 application {
     mainClass.set("net.eplusx.mimosa.daemon.AppKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 // Set the Main-Class attribute in the manifest to make a JAR file executable.
